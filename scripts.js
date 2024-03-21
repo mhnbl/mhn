ecf72351b9eb9b4dca22f7e855e93ac6b7d66ec9
mhnbl <125839542+mhnbl@users.noreply.github.com>
2024-03-21 10:26:22 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -85,4 +85,23 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
+// Go To Top
+var toTopButton = document.getElementById("to-top-button");
+    
+if (toTopButton) {
 
+    window.onscroll = function() {
+        if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+            toTopButton.classList.remove("hidden");
+        } else {
+            toTopButton.classList.add("hidden");
+        }
+    };
+
+    window.goToTop = function() {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    };
+}
